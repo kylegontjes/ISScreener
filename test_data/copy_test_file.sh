37313486e80a202ed3e1b45df2955a47c728af26
kylegontjes <kylegontjes@umich.edu>
2024-05-30 21:05:50 -0400

    add test data

---
+++ test_data/copy_test_file.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# The fastq files are found in the path below
+## For this pipeline, let's start just on one isolate, PCMP_H10, which was predicted to have plenty of insertion sequences
+
+cp /nfs/esnitkin/Project_Penn_KPC/Sequence_data/fastq/Penn/SRA_submission/PCMP_H10_R* .
+cp /nfs/esnitkin/Project_Penn_KPC/Sequence_data/fastq/Penn/SRA_submission/PCMP_H100_R* .
